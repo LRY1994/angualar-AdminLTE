262d434053fdc3bb7d04a95bd51121b7b4359c17
LRY1994 <949892801@qq.com>
2017-03-28 16:23:25 +0800

    退出成功了，直接调用rootscope上面的logout函数，再跳转到login页面即可

--- components/header.js
+++ components/header.js
@@ -1,8 +1,11 @@
 'use strict';
 
-angular.module('com.pupil.app').controller('HeaderController', function () {
- 
-});
+angular.module('com.pupil.app').controller('HeaderController', ['$scope','$state','$rootScope',function ($scope,$state,$rootScope) {
+  $scope.logout = function(){  
+  	$rootScope.logout();
+  	$state.go("login");
+  }
+}]);
 angular.module('com.pupil.app').directive('pHeader', function () {
   return {
     restrict: 'EA',
